9b2e074fc0b5b90f62d41169abc076a53502e35a
Aruni Ranaweera <arunixr@gmail.com>
2019-03-21 01:58:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(*phrase)
+  if phrase.empty?
+    return "Squawk!"
+    p "Squawk!"
+  else
+    return "say #{phrase} say"
+    puts phrase.to_s
+  end
+end
